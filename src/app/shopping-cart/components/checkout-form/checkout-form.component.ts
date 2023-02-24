b47f8b2e3c7e670a756e0ee3ffaa38a9344ef77e
Ernesto González <ernesgonzalez.33.egc@icloud.com>
2023-02-24 10:59:27 +0100

    Add checkout form

---
+++ src/app/shopping-cart/components/checkout-form/checkout-form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-form',
+  templateUrl: './checkout-form.component.html',
+  styleUrls: ['./checkout-form.component.css']
+})
+export class CheckoutFormComponent implements OnInit {
+
+  name: string = '';
+  address: string = '';
+  card: number = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    alert("Thanks for buying with us!");
+  }
+
+}
--- src/app/shopping-cart/shopping-cart.module.ts
+++ src/app/shopping-cart/shopping-cart.module.ts
@@ -4,9 +4,10 @@ import { CartComponent } from './components/cart/cart.component';
 import { CartItemComponent } from './components/cart-item/cart-item.component';
 import { StoreFrontModule } from '../store-front/store-front.module';
 import { FormsModule } from '@angular/forms';
+import { CheckoutFormComponent } from './components/checkout-form/checkout-form.component';
 
 @NgModule({
-  declarations: [CartComponent, CartItemComponent],
+  declarations: [CartComponent, CartItemComponent, CheckoutFormComponent],
   imports: [CommonModule, StoreFrontModule, FormsModule],
 })
 export class ShoppingCartModule {}
